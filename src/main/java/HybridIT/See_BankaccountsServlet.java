f1db75cd91f7bdcb1fe8be2859df490b0d087721
sailakshmiv <lakshmi.saiv99@gmail.com>
2016-08-11 18:02:55 +0530

    Update See_BankaccountsServlet.java

--- src/main/java/HybridIT/See_BankaccountsServlet.java
+++ src/main/java/HybridIT/See_BankaccountsServlet.java
@@ -14,7 +14,7 @@ public class See_BankaccountsServlet extends HttpServlet {
 		private String returnSet;
 	
 		private String DBNAME= "sai";
-		private String HOSTNAME= "127.0.0.1";
+		private String HOSTNAME= "0.0.0.0";
 		private String USER = "root";
 		private String PASSWORD = "root";
 		private String PORT= "3306";
